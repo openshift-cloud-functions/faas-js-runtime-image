19226a0c17465436d59fe796e8298544cde285c9
slinkydeveloper <francescoguard@gmail.com>
2019-08-14 10:30:45 +0200

    Added overload-protection to use as k8s probe

--- run-test.sh
+++ run-test.sh
@@ -15,6 +15,19 @@ remove_node_modules () {
   rm -rf test/false # TODO: What is this dir doing here?
 }
 
+test_probe () {
+  probe_path=$1
+  echo "Testing probe path ${probe_path}"
+  EXPECTED='OK'
+  RESPONSE=$(curl "http://localhost:8080${probe_path}")
+  if [ "${RESPONSE}" == "${EXPECTED}" ] ; then
+    echo "Got expected response from probe ${probe_path} '${RESPONSE}'"
+  else
+    echo "Unexepected response received from probe ${probe_path} '${RESPONSE}'"
+    exit 1
+  fi
+}
+
 cleanup () {
   echo "Stopping ${TEST_IMAGE}..."
   docker stop $(cat faas-test.cid)
@@ -52,6 +65,8 @@ else
   echo "Unexepected response received from function '${RESPONSE}'"
   fail
 fi
+
+test_probe "/health"
 
 echo "Test success. Cleaning up..."
 cleanup
