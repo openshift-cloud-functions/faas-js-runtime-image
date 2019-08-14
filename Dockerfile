c9d28f8d2628421006965c22c984609e6cd5e883
slinkydeveloper <francescoguard@gmail.com>
2019-08-14 10:30:45 +0200

    Added overload-protection to use as k8s probe

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,11 @@ FROM node:12-alpine
 
 EXPOSE 8080
 
-USER 1000
-
 COPY src /home/node/src
 RUN mkdir /home/node/usr
+
+RUN cd /home/node/src && npm install
+
+USER 1000
 
 CMD ["/home/node/src/run.sh"]
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 const http = require('http');
 const Context = require('./context.js');
+const protection = require('overload-protection');
+
+// Default LIVENESS/READINESS urls
+const READINESS_URL = '/health';
+const LIVENESS_URL = '/health';
 
 // load the user function
 const func = require('../usr');
 
+// Configure protect for liveness/readiness probes
+const protectCfg = {
+  production: process.env.NODE_ENV === 'production',
+  maxHeapUsedBytes: 0, // Maximum heap used threshold (0 to disable) [default 0]
+  maxRssBytes: 0, // Maximum rss size threshold (0 to disable) [default 0]
+  errorPropagationMode: false // Don't propagate error to callback, if something is wrong just reply with correct status code
+};
+const readinessURL = process.env.READINESS_URL || READINESS_URL;
+const livenessURL = process.env.LIVENESS_URL || LIVENESS_URL;
+const protect = protection('http', protectCfg);
+
 // listen for incoming requests
 const server = http.createServer((req, res) => {
-  res.end(func(new Context(req, res)));
+  // Check if health path
+  if (req.url === readinessURL || req.url === livenessURL) {
+    protect(req, res, () => res.end("OK"))
+  } else {
+    res.end(func(new Context(req, res)));
+  }
 });
 
 server.on('clientError', (err, socket) => {
