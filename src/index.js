427c08554259f804af40d40d442d82a93b84917d
Lance Ball <lball@redhat.com>
2019-10-18 10:39:36 -0700

    src: extract runtime framework into its own module (#29)
    The runtime framework can be developed tested and published
    independent of the runtime image by pulling the source out
    of this repository and publishing it in the npm repository
    at https://www.npmjs.com/package/@redhat/faas-js-runtime.
    
    The framework source has been copied to another repository
    at https://github.com/openshift-cloud-functions/faas-js-runtime.
    
    Fixes: https://github.com/openshift-cloud-functions/faas-js-runtime-image/issues/28
    
    build: bump image tag to 0.0.2
    
    The 0.0.2 version of the runtime does not install dependencies
    when the framework spins up. That is the responsibility of the
    user. In our case, the assemble script does the npm install for
    any user functions. The application entry point at index.js now
    passes a function instead of a path to a function to the framework.

--- src/index.js
+++ src/index.js
@@ -1,57 +1,10 @@
 'use strict';
 
-const http = require('http');
-const Context = require('./lib/context');
-const eventHandler = require('./lib/event-handler');
-const protection = require('overload-protection');
+const path = require('path');
+const framework = require('@redhat/faas-js-runtime');
 
-// Default LIVENESS/READINESS urls
-const READINESS_URL = '/health';
-const LIVENESS_URL = '/health';
+console.log(`Executing in ${__dirname}`);
 
-// load the user function
-const func = require('../usr');
-
-// Configure protect for liveness/readiness probes
-const protectCfg = {
-  production: process.env.NODE_ENV === 'production',
-  maxHeapUsedBytes: 0, // Maximum heap used threshold (0 to disable) [default 0]
-  maxRssBytes: 0, // Maximum rss size threshold (0 to disable) [default 0]
-  errorPropagationMode: false // Don't propagate error to callback, if something is wrong just reply with correct status code
-};
-const readinessURL = process.env.READINESS_URL || READINESS_URL;
-const livenessURL = process.env.LIVENESS_URL || LIVENESS_URL;
-const protect = protection('http', protectCfg);
-
-// listen for incoming requests
-const server = http.createServer((req, res) => {
-  const context = new Context(req, res);
-  // Check if health path
-  if (req.url === readinessURL || req.url === livenessURL) {
-    protect(req, res, () => res.end("OK"))
-  } else if (('ce-type' in req.headers) && req.headers['ce-type'].startsWith('dev.knative')) {
-    eventHandler(req, res)
-      .then(event => {
-        context.cloudevent = event;
-        res.end(func(context))
-      })
-      .catch(err => {
-        // TODO: This should do some better error handling. What should the caller get?
-        console.error(err);
-        res.end(err);
-      });
-  } else {
-    res.end(func(context));
-  }
+framework(require(path.join(`${__dirname}`, '..', 'usr')), 8080, server => {
+  console.log('FaaS framework initialized.');
 });
-
-server.on('clientError', (err, socket) => {
-  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-  console.log(err);
-});
-
-server.on('listening', _ => {
-  console.log(`Server listening on port ${server.address().port}`);
-});
-
-server.listen(8080);
