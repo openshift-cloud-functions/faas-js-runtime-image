4fd9719669b27a87fe98b54df37c21c401ddb100
Lance Ball <lball@redhat.com>
2019-11-12 15:58:47 -0500

    src: remove stray require statement

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ const path = require('path');
 const framework = require('@redhat/faas-js-runtime');
 const userFunction = require(path.join(`${__dirname},`, '..', 'usr'));
 
-const http = require('http');
-
 framework(userFunction, 8080, server => {
   console.log('FaaS framework initialized');
 });
