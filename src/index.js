c9fe174fe3b0d1a62c6e0a9b854b14a9c55ec2af
Helio Frota <00hf11@gmail.com>
2020-04-17 15:15:06 -0300

    fix: close on SIGTERM and SIGINT signals

--- src/index.js
+++ src/index.js
@@ -7,3 +7,20 @@ const userFunction = require(path.join(`${__dirname},`, '..', 'usr'));
 framework(userFunction, 8080, server => {
   console.log('FaaS framework initialized');
 });
+
+process.stdin.resume();
+
+let confirm = false;
+process.on('SIGINT', signal => {
+  if (confirm) {
+    process.exit(130);
+  } else {
+    console.log('Press Control+C again to confirm.');
+    confirm = true;
+  }
+});
+
+process.on('SIGTERM', signal => {
+  console.log(`Received ${signal}`);
+  process.exit(143);
+});
